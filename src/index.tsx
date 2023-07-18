aa6e4c17183ad8011f2579bbbeeac3679c5cc3f5
Ilya Kruchinin <kr4chinin@mail.ru>
2023-07-18 14:02:29 +0300

    chore: fix tsconfig, remove .tsx in index imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
-import { SystemModalContextProvider } from './context/SystemModalContextProvider.tsx';
+import { App } from './App';
+import { SystemModalContextProvider } from './context/SystemModalContextProvider';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
